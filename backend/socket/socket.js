0effcbfa3e9d82e7be9308493698743d50f4b911
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-07-12 11:53:29 +0530

    Remove socket handler implementation for user connection and disconnection events

--- backend/socket/socket.js
+++
@@ -1,9 +0,0 @@
-
-export const socketHandler = (io) => {
-  io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
-    socket.on("disconnect", () => {
-      console.log("A user disconnected:", socket.id);
-    });
-  });
-};
